ee2c869c2356cc62f4fa780ef1aa5579a733c134
SaJiL S <sajilsajeev5726@gmail.com>
2026-02-13 14:33:01 +0530

    Create cpu.java

---
+++ cpu.java
@@ -0,0 +1,24 @@
+class CPU
+double price=15000.0;
+class Processor llinner class
+Il members of nested class int cores=10;
+Sring manufacturer='Pentium";
+statiç class RAM Ilstatic nested class
+I/ members of staticnested class double memory=64;
+String manufacture="Intel";
+public class ÇPUDetails
+public static void main ( Stingl] args)
+CPU c=new CPU():
+CPU PrceEsa.p=gue Processo();CPURAMr=neCPU.RAM);System. out prinln("CPU class');System.out pinthn("-....
+Sistem.out.pinta("CPU Pice="ts.pise):
+1/create object of Outer class CPU
+Il create an object ofinner class Processor using outer class
+1lcreate an objectof static class RAMusing outerclass CPU
+Sxstem.out printdn("Processorinner clas"):
+Sustem.out pinln(".-..-"):
+Sxstemout pritla("No.ofcores =" tp.cores):
+Sustenm.out pinda("Manufacturer="tp.manufacture):
+System out prindn("RAMstatic nested class");
+Ssstem.out pintho(".........
+Systenm.out pinln("Memory=" t.memom);
+System out prinda("Manufactarer=" tt .manufaturer);
